c660d71f7a8d5f94faeac05bde4d39954df5e3a8
Felipe Sasso <felipecsasso@gmail.com>
2019-12-03 17:55:06 -0800

    Finished day 1 challenged. Added new methods

--- day1.py
+++ day1.py
@@ -1,11 +1,41 @@
-from utils import get_filename_from_arg
+from utils import get_filename_from_arg, get_int_list_from_file
 
-def get_fuel_requirements(filename):
-    with open(filename) as file:
-        fuel = sum(map(lambda x: (int(x) // 3) - 2, file.read().split()))
+def get_total_fuel_fuel_requirements(mass_list):
+    return sum(map(get_fuel_fuel_requirements, mass_list))
 
-    return fuel
+def get_fuel_fuel_requirements(mass):
+
+    requirements = 0
+    if mass <= 0:
+        raise ValueError('Mass must be a positive number')
+
+    while True:
+        mass = get_rocket_fuel_requirements(mass)
+        if mass <= 0:
+            return requirements
+        requirements += mass
+
+def get_rocket_fuel_requirements(mass):
+    if type(mass) != int:
+        if not mass.is_digit():
+            raise ValueError('Mass must be a digit!')
+            mass = int(mass)
+    if mass <= 0:
+        raise ValueError('Mass must be a positive number')
+    return (mass // 3) - 2
+
+def get_total_fuel_requirements(mass_list):
+    return sum(map(get_rocket_fuel_requirements, mass_list))
+
+def get_total_fuel_requirements_from_file(filename):
+    list_ = get_int_list_from_file(filename)
+    return get_total_fuel_requirements(list_)
+
+def get_total_fuel_fuel_requirements_from_file(filename):
+    list_ = get_int_list_from_file(filename)
+    return get_total_fuel_fuel_requirements(list_)
 
 if __name__ == '__main__':
     filename = get_filename_from_arg()
-    print(get_fuel_requirements(filename))
+    print('Part 1: {}'.format(get_total_fuel_requirements_from_file(filename)))
+    print('Part 2: {}'.format(get_total_fuel_fuel_requirements_from_file(filename)))
