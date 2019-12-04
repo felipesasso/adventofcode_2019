4a7751c28ef8a208bfbd1045bc3976aa9286b349
Felipe Sasso <felipecsasso@gmail.com>
2019-12-03 17:55:39 -0800

    Added new method to read masses from file

--- utils.py
+++ utils.py
@@ -5,3 +5,9 @@ def get_filename_from_arg():
     if len(args) < 1 and len(args) > 2:
         raise ValueError('Only one argument accepted')
     return args[1]
+
+def get_int_list_from_file(filename, sep=None):
+    with open(filename) as f:
+        list_ = list(map(int, f.read().split(sep)))
+
+    return list_
